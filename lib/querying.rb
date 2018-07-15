0f6fd2e3a9fc5bb9f67cd2985fd9d50e418f2a81
Ariel Zhen <zhenpingping@live.com>
2018-07-15 05:34:06 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -11,7 +11,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT characters.species, COUNT(characters.species) AS most_prolific 
+  "SELECT characters.species, COUNT(characters.species) AS most_prolific
     FROM     characters
     GROUP BY species
     ORDER BY most_prolific DESC
