59a522574a7abae58ca842cbaaa96214b3ebd196
Ariel Zhen <zhenpingping@live.com>
2018-07-15 05:34:01 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,18 +1,25 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "SELECT books.title, books.year FROM books
+   WHERE books.series_id = 1
+   ORDER BY books.year;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT characters.name, characters.motto FROM characters
+   ORDER BY characters.motto LIMIT 1;"
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT characters.species, COUNT(characters.species) AS most_prolific 
+    FROM     characters
+    GROUP BY species
+    ORDER BY most_prolific DESC
+    LIMIT    1;"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name, "
 end
 
 def select_series_title_with_most_human_characters
@@ -20,5 +27,5 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name, COUNT()"
 end
